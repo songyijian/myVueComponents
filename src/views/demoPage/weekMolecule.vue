0703ce6e3998b068f34d85a8d41b177b56f396e7
yijian.song <yijian.song@opay-inc.com>
2021-07-08 19:25:48 +0800

    chore:..

--- src/views/demoPage/weekMolecule.vue
+++ src/views/demoPage/weekMolecule.vue
@@ -4,19 +4,17 @@
  * @Version: 0.1.0
  * @Date: 2021-07-07 19:59:52
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-07-08 16:28:06
+ * @LastEditTime: 2021-07-08 19:18:36
 -->
 
 <template>
   <div>
     <h2>WeekMolecule 周小时粒度管理控件</h2>
 
-    <WeekMolecule :disabled="disabled"></WeekMolecule>
+    <p><button @click="allSelect">全选</button></p>
     <WeekMolecule v-model="val"></WeekMolecule>
-
-    <button @click="allSelect">全选</button>
-
-    {{ val }}
+    <p>disabled</p>
+    <WeekMolecule :disabled="disabled"></WeekMolecule>
   </div>
 </template>
 
