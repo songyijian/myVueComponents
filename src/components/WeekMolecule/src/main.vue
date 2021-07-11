dd5507f17746ed4237d3cbbd29b660eac7607524
songyijian <397032724@qq.com>
2021-07-12 00:55:59 +0800

    feat:样式

--- src/components/WeekMolecule/src/main.vue
+++ src/components/WeekMolecule/src/main.vue
@@ -4,78 +4,65 @@
  * @Version: 0.1.0
  * @Date: 2021-07-07 15:51:39
  * @LastEditors: yijian.song
- * @LastEditTime: 2021-07-09 00:43:52
+ * @LastEditTime: 2021-07-12 00:53:54
 -->
 <template lang="html">
-  <div class="schedule_box">
-    <!-- {{ fitStatue }} -->
-    <input type="hidden" :value="value" />
-    <div :class="{ disabledstyle: disabled }" :style="{
-        width: `${tdSize * 24 + dayWidth}px`,
-      }">
-      <div class="time_title_box" :style="{
-          'margin-left': dayWidth + 'px',
-          height: timeHeight + 'px'
-        }">
-        <span v-for="(item, index) in time" :key="'t' + index" :data-index="index" @click="timeTitle(index)" :style="{
-            width: `${tdSize}px`,
-            'line-height': timeHeight + 'px'
-          }">
+  <div class="week-molecule" :class="{ disabledstyle: disabled }">
+    <div class="body">
+      <div class="column">
+        <span v-for="(item, index) in time" :key="item" @click="columnSelect(index)" :style="{width: `${size}px`}">
           {{ item }}
         </span>
       </div>
-      <div class="schedule_body_box">
-        <div class="day_title_box" :style="{ width: dayWidth + 'px' }">
-          <span v-for="(item, index) in day" :key="item" @click="dayTitle(index)" :style="{
-              height: `${tdSize}px`,
-              'line-height': `${tdSize}px`
+      <div class="row">
+        <span v-for="(item, index) in day" :key="item" @click="rowSelect(index)" :style="{
+              height: `${size}px`,
+              'line-height': `${size}px`
             }">
-            {{ item }}
-          </span>
-        </div>
-        <div ref="time_bloack_box" class="time_bloack_box" @mousedown="down($event)" @mouseup="up($event)"
-          @mousemove="move($event)" @mouseleave="leave($event)">
-          <div v-for="(itemf, indexf) in fitStatue" :key="'f' + indexf" :style="{ height: `${tdSize}px` }">
-            <span v-for="(item, index) in itemf" :style="{
-                width: `${tdSize}px`,
-                height: `${tdSize}px`
-              }" :key="'c' + index + indexf" :class="{ schedule_time_item_yes: item == 1 }">
-              {{ index }}
-            </span>
-            <i v-for="(item, index) in txtShowStatue[indexf]" :style="{
-                height: `${tdSize}px`,
-                'line-height': `${tdSize}px`,
-                width: `${item.length * tdSize}px`,
-                left: `${item.start * tdSize}px`
-              }">
-              {{ item.length == 24 ? "全天投放" : `${item.start}:00 - ${item.end + 1}:00` }}
-            </i>
-          </div>
-          <em class="move_bg" :style="[boxSelectionStyle]"></em>
-        </div>
+          {{ item }}
+        </span>
       </div>
-      <div class="schedule_flter_box" :style="{
-          height: flterHeight + 'px',
-          'line-height': flterHeight + 'px'
-        }">
-        <div class="flter_btn_box">
-          快速设定：
-          <span @click="dayTf('all')">全周</span>
-          <span @click="dayTf('restDay')">周一至周五</span>
-          <span @click="dayTf('workingDay')">周六日</span>
-        </div>
-        <div class="flter_msg_box">
-          <span>
-            <i class="play_b"></i>
-            有效时间
+      <div ref="molecule" class="molecule" @mousedown="down($event)" @mouseup="up($event)" @mousemove="move($event)"
+        @mouseleave="leave($event)">
+        <div v-for="(itemf, indexf) in fitStatue" :key="'f' + indexf" :style="{ height: `${size}px` }">
+          <span v-for="(item, index) in itemf" :style="{
+                width: `${size}px`,
+                height: `${size}px`
+              }" :key="'c' + index + indexf" :class="{ isselect: item == 1 }">
+            {{ index }}
           </span>
-          <span>
-            <i class="stop_b"></i>
-            无效时间
-          </span>
+          <i v-for="(item, index) in infoStatue[indexf]" :style="{
+                height: `${size}px`,
+                'line-height': `${size}px`,
+                width: `${item.length * size}px`,
+                left: `${item.start * size}px`
+              }">
+            {{ item.length == 24 ? "全天" : `${item.start}:00 - ${item.end + 1}:00` }}
+          </i>
         </div>
+        <em class="box-select-style" :style="[boxSelectionStyle]"></em>
       </div>
     </div>
+
+    <div class="schedule_flter_box">
+      快速设定：
+      <div class="flter_btn_box">
+        <button @click="dayTf('all')">全周</button>
+        <button @click="dayTf('restDay')">周一至五</button>
+        <button @click="dayTf('workingDay')">周六日</button>
+      </div>
+      <div class="flter_msg_box">
+        <span>
+          <i class="play_b"></i>
+          有效时间
+        </span>
+      </div>
+    </div>
+    <input type="hidden" :value="value" />
+
+
+    <el-calendar v-model="tnow">
+    </el-calendar>
   </div>
 </template>
 
@@ -103,27 +90,9 @@
           return Array(24 * 7).fill(0);
         }
       },
-      tdSize: {
+      size: {
         type: Number,
-        default: 26,
-        validator(val) {
-          return val >= 16;
-        }
-      },
-      dayWidth: {
-        type: Number,
-        default: 50
-      },
-      timeHeight: {
-        type: Number,
-        default: 30,
-        validator(val) {
-          return val >= 16;
-        }
-      },
-      flterHeight: {
-        type: Number,
-        default: 30,
+        default: 32,
         validator(val) {
           return val >= 16;
         }
@@ -136,6 +105,7 @@
     data() {
       let getSate = inState(this.value);
       return {
+        tnow: new Date(),
         boxSelectionStyle: {
           display: "none"
         },
@@ -146,7 +116,7 @@
       };
     },
     computed: {
-      txtShowStatue: function () {
+      infoStatue: function () {
         return this.fitStatue.map(item => getRepeat(item));
       },
       resetValue: function () {
@@ -161,7 +131,7 @@
     },
     methods: {
       /** 24小时快速选择 */
-      timeTitle(index) {
+      columnSelect(index) {
         if (this.disabled) return;
 
         let nums = copy(this.fitStatue);
@@ -173,7 +143,7 @@
         this.$emit("input", undoState(nums));
       },
       /** 星期1～7快速选择 */
-      dayTitle(index) {
+      rowSelect(index) {
         if (this.disabled) return;
         let nums = copy(this.fitStatue);
         let state = nums[index].indexOf(0) >= 0;
@@ -206,7 +176,7 @@
         this.downOff = true;
         this.scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
         this.scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
-        this.boxData = this.$refs.time_bloack_box.getBoundingClientRect();
+        this.boxData = this.$refs.molecule.getBoundingClientRect();
         let y = ev.pageY - this.boxData.y - this.scrollTop;
         let x = ev.pageX - this.boxData.x - this.scrollLeft;
         this.downData = {
@@ -215,23 +185,21 @@
         };
         // 鼠标位于bg磁性坐标于的偏移量
         this.dowBgDval = {
-          top: y % this.tdSize,
-          left: x % this.tdSize,
-          bottom: this.tdSize - (y % this.tdSize),
-          right: this.tdSize - (x % this.tdSize)
+          top: y % this.size,
+          left: x % this.size,
+          bottom: this.size - (y % this.size),
+          right: this.size - (x % this.size)
         };
         this.boxSelectionStyle = {
           top: y - this.dowBgDval.top + "px",
           left: x - this.dowBgDval.left + "px",
-          width: this.tdSize + "px",
-          height: this.tdSize + "px",
+          width: this.size + "px",
+          height: this.size + "px",
           display: "inline-block"
         };
       },
       move(ev) {
-        if (this.disabled) {
-          return;
-        }
+        if (this.disabled) return;
         /** 鼠标移动 (不会存在 yl === 0 ；xl === 0 的清空) */
         if (this.downOff) {
           //移动坐标处理
@@ -246,8 +214,8 @@
           // 变量抽离
           let top = this.downData.y - this.dowBgDval.top + "px";
           let left = this.downData.x - this.dowBgDval.left + "px";
-          let w = this.tdSize + "px";
-          let h = this.tdSize + "px";
+          let w = this.size + "px";
+          let h = this.size + "px";
           /** 顺势框选
                   yl > 0  向下移动鼠标
                   xl > 0  向右滑动
@@ -255,22 +223,22 @@
           if (yl > 0) {
             if (y > this.downData.y + this.dowBgDval.bottom) {
               // 有效移动
-              if (y < this.downData.y + this.dowBgDval.bottom + this.tdSize) {
+              if (y < this.downData.y + this.dowBgDval.bottom + this.size) {
                 //移动到下一格范围，但实距离<1个单元格距离
-                h = this.tdSize * 2 + "px";
+                h = this.size * 2 + "px";
               } else {
                 //已经达到通过移动单元格距离来计算宽度： h = 实际距离/格子宽度 + 半个磁性偏移量
-                h = (Math.ceil((yl - this.dowBgDval.bottom) / this.tdSize) + 1) * this.tdSize + "px";
+                h = (Math.ceil((yl - this.dowBgDval.bottom) / this.size) + 1) * this.size + "px";
               }
             }
           }
           if (xl > 0) {
             if (x > this.downData.x + this.dowBgDval.right) {
               // 有效移动
-              if (x < this.downData.x + this.dowBgDval.right + this.tdSize) {
-                w = this.tdSize * 2 + "px";
+              if (x < this.downData.x + this.dowBgDval.right + this.size) {
+                w = this.size * 2 + "px";
               } else {
-                w = (Math.ceil((xl - this.dowBgDval.right) / this.tdSize) + 1) * this.tdSize + "px";
+                w = (Math.ceil((xl - this.dowBgDval.right) / this.size) + 1) * this.size + "px";
               }
             }
           }
@@ -278,23 +246,23 @@
           if (yl < 0) {
             if (y < this.downData.y - this.dowBgDval.top) {
               // 有效移动
-              if (y > this.downData.y - this.dowBgDval.top - this.tdSize) {
-                h = this.tdSize * 2 + "px";
+              if (y > this.downData.y - this.dowBgDval.top - this.size) {
+                h = this.size * 2 + "px";
               } else {
-                h = (Math.ceil((ylAbs - this.dowBgDval.top) / this.tdSize) + 1) * this.tdSize + "px";
+                h = (Math.ceil((ylAbs - this.dowBgDval.top) / this.size) + 1) * this.size + "px";
               }
-              top = Math.floor(y / this.tdSize) * this.tdSize + "px";
+              top = Math.floor(y / this.size) * this.size + "px";
             }
           }
           if (xl < 0) {
             if (x < this.downData.x - this.dowBgDval.left) {
               // 有效移动
-              if (x > this.downData.x - this.dowBgDval.left - this.tdSize) {
-                w = this.tdSize * 2 + "px";
+              if (x > this.downData.x - this.dowBgDval.left - this.size) {
+                w = this.size * 2 + "px";
               } else {
-                w = (Math.ceil((xlAbs - this.dowBgDval.left) / this.tdSize) + 1) * this.tdSize + "px";
+                w = (Math.ceil((xlAbs - this.dowBgDval.left) / this.size) + 1) * this.size + "px";
               }
-              left = Math.floor(x / this.tdSize) * this.tdSize + "px";
+              left = Math.floor(x / this.size) * this.size + "px";
             }
           }
           this.boxSelectionStyle = {
@@ -320,10 +288,10 @@
       },
       mousefn() {
         if (this.disabled) return;
-        const dayStart = parseInt(this.boxSelectionStyle.top) / this.tdSize; //天数组索引 start
-        const dayEnd = parseInt(this.boxSelectionStyle.height) / this.tdSize + dayStart - 1; //天数组索引 end
-        const timeStart = parseInt(this.boxSelectionStyle.left) / this.tdSize; //小时数组（子）索引 start
-        const timeEnd = parseInt(this.boxSelectionStyle.width) / this.tdSize + timeStart - 1; //小时数组（子）索引 end
+        const dayStart = parseInt(this.boxSelectionStyle.top) / this.size; //天数组索引 start
+        const dayEnd = parseInt(this.boxSelectionStyle.height) / this.size + dayStart - 1; //天数组索引 end
+        const timeStart = parseInt(this.boxSelectionStyle.left) / this.size; //小时数组（子）索引 start
+        const timeEnd = parseInt(this.boxSelectionStyle.width) / this.size + timeStart - 1; //小时数组（子）索引 end
 
         let nums = copy(this.fitStatue);
         for (let i = dayStart; i <= dayEnd; i++) {
@@ -342,15 +310,14 @@
   };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   $color: #409eff;
   $selcet-cover: rgba(64, 158, 255, 0.7);
-  $border-color: #dcdfe6;
-  $bg-color:#f5f7fa;
+  $border-color: #EBEEF5;
+  $bg-color: #fff; //#f5f7fa;
 
-  .schedule_box {
-    /* padding: 2px; */
-    /* background: #f5f5f5db; */
+  .week-molecule {
+    background-color: #fff;
     display: inline-block;
     position: relative;
     overflow: hidden;
@@ -371,70 +338,75 @@
     }
   }
 
-
-  /* 0~24 */
-  .time_title_box {
+  .body {
+    border: 1px solid $border-color;
+    overflow: hidden;
     font-size: 0;
 
-    span {
-      font-size: 9px;
-      display: inline-block;
+    .column {
+      font-size: 0;
+      /* text-align: right; */
+      padding-left: 35px;
+      height: 25px;
+      line-height: 25px;
+      font-size: 12px;
+
+      &>span {
+        height: 100%;
+        display: inline-block;
+        text-align: center;
+        border-left: 1px solid #eaeefb;
+      }
+    }
+
+    .row {
+      float: left;
+      width: 35px;
       text-align: center;
-      border: 1px solid #eaeefb;
-      border-left: none;
-      line-height: 20px;
+
+      span {
+        display: inline-block;
+        width: 100%;
+        font-size: 9px;
+        border-top: 1px solid $border-color;
+      }
+    }
+
+  }
+
+
+  .molecule {
+    float: left;
+    position: relative;
+
+    span {
+      font-size: 0;
+      height: 25px;
+      display: inline-block;
+      border-left: 1px solid $border-color;
+      border-top: 1px solid $border-color;
+      background: $bg-color;
+
+      &:hover {
+        border: 1px solid rgb(63, 136, 245);
+        background-color: #F2F8FE;
+      }
     }
   }
 
 
-  /* 主体 */
-  .schedule_body_box {
-    overflow: hidden;
-    font-size: 0;
-  }
 
-  /* 周 1～7 */
-  .day_title_box {
-    display: flex;
-    float: left;
-    flex-flow: column nowrap;
-    text-align: center;
-  }
-
-  .day_title_box>span {
-    font-size: 9px;
-    border: 1px solid $border-color;
-  }
-
-  /* 块 */
-  .time_bloack_box {
-    /* border: 2px solid; */
-    float: left;
-    position: relative;
-  }
-
-  .time_bloack_box span {
-    font-size: 0;
-    height: 25px;
-    display: inline-block;
-    border-right: 1px solid $border-color;
-    border-bottom: 1px solid $border-color;
-    background: $bg-color;
-    /* &:hover {
-      background: #ecf5ff;
-    } */
-  }
-
-  .time_bloack_box .schedule_time_item_yes {
+  .molecule .isselect {
     background-color: $color;
     border-color: #b3d8ff
   }
 
-  .time_bloack_box>div {
+  .molecule>div {
     position: relative;
   }
 
-  .time_bloack_box>div>i {
+  .molecule>div>i {
+    pointer-events: none;
     display: inline-block;
     position: absolute;
     top: 0;
@@ -444,13 +416,11 @@
     color: #fff;
     background: $selcet-cover;
     box-sizing: border-box;
-    /* &:hover {
-      background-color: rgba(64, 158, 255, 0.3);
-    } */
+
   }
 
 
-  em.move_bg {
+  em.box-select-style {
     background: rgba(0, 0, 0, 0.6);
     position: absolute;
     display: none;
@@ -503,9 +473,39 @@
   .flter_btn_box {
     float: left;
     line-height: inherit;
+    /* border: 1px solid red; */
+    border: 1px solid #DCDFE6;
+
+    overflow: hidden;
+    border-radius: 3px;
+
+
+
+    button {
+      min-width: 70px;
+      background-color: #fff;
+      border: none;
+      border-radius: 0;
+
+
+      display: inline-block;
+      line-height: 1;
+      white-space: nowrap;
+      cursor: pointer;
+      background: #FFF;
+      /* border: 1px solid #DCDFE6; */
+      color: #606266;
+      -webkit-appearance: none;
+      text-align: center;
+      box-sizing: border-box;
+
+      font-size: 12px;
+      /* border-radius: 3px; */
+      padding: 7px 15px;
+    }
   }
 
-  .flter_btn_box>span {
+  /* .flter_btn_box>span {
     display: inline-block;
     line-height: 0.8;
     white-space: nowrap;
@@ -525,18 +525,18 @@
     -webkit-user-select: none;
     -ms-user-select: none;
     user-select: none;
-  }
+  } */
 
-  .time_title_box>span,
+  .column>span,
   .flter_btn_box>span,
-  .day_title_box>span,
-  .time_bloack_box {
+  .row>span,
+  .molecule {
     cursor: pointer;
     background: #fff;
   }
 
-  .time_title_box>span:hover,
-  .day_title_box>span:hover,
+  .column>span:hover,
+  .row>span:hover,
   .flter_btn_box>span {
     color: $color;
     background: #ecf5ff;
